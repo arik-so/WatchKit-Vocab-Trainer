e8b39b3a63c9b368a054f1a4defd287f11f025f2
Arik Sosman <arik@sosman.eu>
2015-01-10 14:50:07 +0100

    added watchkit target

---
+++ WatchKit Vocab Trainer WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  WatchKit Vocab Trainer WatchKit Extension
+//
+//  Created by Arik Sosman on 1/10/15.
+//  Copyright (c) 2015 WKH. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ WatchKit Vocab Trainer WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  WatchKit Vocab Trainer WatchKit Extension
+//
+//  Created by Arik Sosman on 1/10/15.
+//  Copyright (c) 2015 WKH. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
